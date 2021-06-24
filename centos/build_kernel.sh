589f0ad841833bfb2ea52fb20c30fe0a0e11fdeb
lhwarthas <lhwarthas@163.com>
2021-06-24 22:58:43 +0800

    script change

--- centos/build_kernel.sh
+++ centos/build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CPUS=`cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l`
+CPUS=`cat /proc/cpuinfo| grep "processor" | wc -l`
 source /opt/rh/devtoolset-9/enable
 mkdir -p /root/outkernelrpm
 cd /root
@@ -8,6 +8,5 @@ wget https://mirrors.ustc.edu.cn/kernel.org/linux/kernel/v${LARGE_VER}.x/linux-$
 tar -xvf linux-${LINUX_VERSION}.tar.xz
 cd /root/linux-${LINUX_VERSION}
 cp /root/config/config /root/linux-${LINUX_VERSION}/.config
-make silentoldconfig
 make rpm-pkg -j${CPUS}
 cp /root/rpmbuild/RPMS/x86_64/*.rpm /root/outkernelrpm
--- ubuntu/build_kernel.sh
+++ ubuntu/build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CPUS=`cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l`
+CPUS=`cat /proc/cpuinfo| grep "processor" | wc -l`
 mkdir -p /root/outkerneldeb
 cd /root
 LARGE_VER=`echo $LINUX_VERSION | cut -d "." -f1`
@@ -7,6 +7,5 @@ wget https://mirrors.ustc.edu.cn/kernel.org/linux/kernel/v${LARGE_VER}.x/linux-$
 tar -xvf linux-${LINUX_VERSION}.tar.xz
 cd /root/linux-${LINUX_VERSION}
 cp /root/config/config /root/linux-${LINUX_VERSION}/.config
-make silentoldconfig
 make deb-pkg -j${CPUS}
 cp /root/*.deb /root/outkerneldeb
