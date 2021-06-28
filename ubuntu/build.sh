7c6da10bdbf2734c661b191f6f9b8da2505db517
lhwarthas <lhwarthas@163.com>
2021-06-28 11:40:05 +0800

    clean

--- ubuntu/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-chmod a+x *.sh
-docker build --build-arg kernel_ver=5.12.13 -t ubuntu-kernel-build .
-mkdir artifacts
-docker run --rm -v $(pwd)/artifacts:/root/outkerneldeb ubuntu-kernel-build
-docker rmi ubuntu-kernel-build
